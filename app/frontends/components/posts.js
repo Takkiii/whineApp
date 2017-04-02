28c3000add0b34c8df384e9a0c1f326497106cd4
瀧ヶ崎翔 <sho060110@gmail.com>
2017-04-02 14:08:12 +0900

    :sparkles: Create tableHeader

--- app/frontends/components/posts.js
+++ app/frontends/components/posts.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom'
 
 class Posts extends React.Component {
 	render() {
-		const headers = ["タイトル", "著者", "出版社", "出版年",]
+		const headers = ["タイトル", "著者", "言語", "出版年", "売上部数",]
 		return (
 			<div>
 				<h1>WhineApp</h1>
 				<table>
 					<thead>
 						<tr>
-							{headers.map((header) => {
+							{headers.map((header, index) => {
 								return (
-									<th>{header}</th>
+									<th key={index}>{header}</th>
 								)
 							})}
 						</tr>
@@ -24,6 +24,6 @@ class Posts extends React.Component {
 }
 
 ReactDOM.render(
-	React.createElement(Posts),
+	<Posts />,
 	document.getElementById("posts")
 )
