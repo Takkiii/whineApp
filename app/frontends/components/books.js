094ed42efe4505b4bab691ef40350fb8d28fc42d
瀧ヶ崎翔 <s.takkiii.com@gmail.com>
2017-04-09 16:00:19 +0900

    :sparkles: Add superAgenet

--- app/frontends/components/books.js
+++ app/frontends/components/books.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import request from 'superagent'
 
 class Books extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			books: [],
+		}
+	}
+
+	componentWillMount() {
+		this.getState()
+	}
+
+	getState() {
+		request
+			.get('/api/books/index')
+			.end((error, res) => {
+				if (!error && res.status === 200) {
+					const json = JSON.parse(res.text)
+					this.setState({
+						books: json.books,
+					})
+				} else {
+					reject(res)
+				}
+			})
+	}
+
 	render() {
-		const headers = ["タイトル", "著者", "出版年",]
+		const {books} = this.state
+		const headers = books[0] != null ? Object.keys(books[0]) : []
 		return (
 			<div>
 				<h1>BookApp</h1>
@@ -17,6 +45,19 @@ class Books extends React.Component {
 							})}
 						</tr>
 					</thead>
+					<tbody>
+						{books.map((book, index) => {
+							return (
+								<tr key={index}>
+									{Object.values(book).map((row, index) => {
+										return (
+											<td key={index}>{row}</td>
+										)
+									})}
+								</tr>
+							)
+						})}
+					</tbody>
 				</table>
 			</div>
 		)
